d3989d26e72e604a4bfbe80beb4f8c37c2aa7f72
DY-MALIS <dymalis88@gmail.com>
2026-06-05 16:03:06 +0700

    fix: disable dom text translator crash

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { AppTextTranslator } from "@/components/app-text-translator";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <AppTextTranslator />
       {children}
     </SessionProvider>
   );
